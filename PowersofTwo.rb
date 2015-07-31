8b9fc42e1c9042ef9e17b4997343bd1827b1aaf1
twotaste <sfsf713@nate.com>
2015-07-31 21:01:29 +0900

    Create PowersofTwo.rb

---
+++ PowersofTwo.rb
@@ -0,0 +1,14 @@
+def PowersofTwo(num)
+
+  # code goes here
+  if Math.sqrt(num).ceil == Math.sqrt(num)
+    return true
+  else
+    return false
+  end 
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+PowersofTwo(STDIN.gets)  
+
